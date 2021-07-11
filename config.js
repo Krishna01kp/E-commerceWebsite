50184735bc74aae4e5d4d9e9a35ee8af70adf054
Krishna01kp <krishnakantp033@gmail.com>
2021-07-12 03:22:40 +0530

    E-comm Website

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const PORT =process.env.port || 5555;
+const PORT =process.env.PORT || 5555;
 const MONGO_URL = "mongodb://localhost:27017/";
 const DB_NAME = "ecom";
 const DB_URL = "mongodb+srv://Onlinedb_ecom:OnlineEcom@todoapp.7caeg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
