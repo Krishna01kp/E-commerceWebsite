103381727b8f724cc4cfc5e5e5342488c7b52439
Krishna01kp <krishnakantp033@gmail.com>
2021-07-12 02:59:39 +0530

    A E-comm Website withe Authentication

---
+++ config.js
@@ -0,0 +1,6 @@
+const PORT = 5555;
+const MONGO_URL = "mongodb://localhost:27017/";
+const DB_NAME = "ecom";
+const DB_URL = mongodb+srv://Onlinedb_ecom:OnlineEcom@todoapp.7caeg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority;
+
+module.exports = { PORT, DB_URL };
---
+++ models/product-model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const MODEL_NAME = 'product';
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    image_url: String,
+    price: {
+        type: Number,
+        required: true
+    },
+    description: String,
+    email: {
+        type: String,
+        required: true
+    }
+});
+
+mongoose.model(MODEL_NAME, ProductSchema);
+module.exports = { MODEL_NAME, ProductSchema };
